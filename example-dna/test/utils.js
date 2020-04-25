b188b979e3ed2868a4e81f1bf7b344ed786353f1
guillem <guillem.cordoba@gmail.com>
2020-04-25 18:57:41 +0200

    Fixed tests

--- example-dna/test/utils.js
+++ example-dna/test/utils.js
@@ -1,11 +1,14 @@
 const createGame = (caller) => (rival) =>
   caller.call("tictactoe", "tictactoe", "create_game", {
-    entry: { rival, timestamp: Date.now() / 1000 },
+    rival,
+    timestamp: Date.now() / 1000,
   });
 
 const createMove = (caller) => (gameAddress, x, y) =>
   caller.call("tictactoe", "tictactoe", "create_game", {
-    entry: { game_address: gameAddress, x, y },
+    game_address: gameAddress,
+    x,
+    y,
   });
 
 module.exports = {
