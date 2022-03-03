5c69d3b364e28da009063092a3f7e4eb27c4f630
Emmanuel <aeperez.prez4@gmail.com>
2022-03-03 14:35:30 -0400

    Upgrade hdk to 122 and nix-shell

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 let
-  holonixRev = "843c86d5871df8de2d9a02bdd797ad6f33fa5be5";
+  holonixRev = "46297548fd8db19bec228171a524330e7b5f6744";
   holonixPath = builtins.fetchTarball "https://github.com/holochain/holonix/archive/${holonixRev}.tar.gz";
   holonix = import (holonixPath) {
     holochainVersionId = "v0_0_124";
